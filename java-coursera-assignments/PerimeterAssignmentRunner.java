bba369b15186b9d9031fde88cec023534db12da2
ashok997 <ashok997@gmail.com>
2020-04-28 11:39:42 -0500

    assignment 1 of coursera java programming

---
+++ java-coursera-assignments/PerimeterAssignmentRunner.java
@@ -0,0 +1,128 @@
+import edu.duke.*;
+import java.io.File;
+
+public class PerimeterAssignmentRunner {
+    public double getPerimeter (Shape s) {
+        // Start with totalPerim = 0
+        double totalPerim = 0.0;
+        // Start wth prevPt = the last point 
+        Point prevPt = s.getLastPoint();
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+            // Update totalPerim by currDist
+            totalPerim = totalPerim + currDist;
+            // Update prevPt to be currPt
+            prevPt = currPt;
+        }
+        // totalPerim is the answer
+        return totalPerim;
+    }
+
+    public int getNumPoints (Shape s) {
+        int totalPoints = 0;
+        
+        Point prevPt =s.getLastPoint();
+        
+        for (Point currPt : s.getPoints()){
+            totalPoints ++;
+        }
+        return totalPoints;
+    }
+
+    public double getAverageLength(Shape s) {
+        double avgLength = getPerimeter(s)/getNumPoints(s);
+        
+        return avgLength;
+    }
+
+    public double getLargestSide(Shape s) {
+        double largestSide =0.0;
+        Point prevPt = s.getLastPoint();
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+            if (currDist > largestSide){
+                largestSide = currDist;
+            }
+            prevPt = currPt;
+        }
+        return largestSide;
+    }
+
+    public double getLargestX(Shape s) {
+        double largestX = 0.0;
+        Point prevPt = s.getLastPoint();
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currX = currPt.getX();
+            if (currX > largestX){
+                largestX = currX;
+            }
+            prevPt = currPt;
+        }
+        return largestX;
+    }
+
+    public double getLargestPerimeterMultipleFiles() {
+        // Put code here
+        return 0.0;
+    }
+
+    public String getFileWithLargestPerimeter() {
+        // Put code here
+        File temp = null;    // replace this code
+        return temp.getName();
+    }
+
+    public void testPerimeter () {
+        FileResource fr = new FileResource();
+        Shape s = new Shape(fr);
+        double length = getPerimeter(s);
+        int points = getNumPoints(s);
+        double aveLength = getAverageLength(s);
+        double largestSide = getLargestSide(s);
+        double largestX = getLargestX(s);
+        
+        System.out.println("perimeter = " + length);
+        System.out.println("No of points = " + points);
+        System.out.println("Average Length = " + aveLength);
+        System.out.println("Largest Side =" + largestSide);
+        System.out.println("Largest X =" + largestX);
+    }
+    
+    public void testPerimeterMultipleFiles() {
+        // Put code here
+    }
+
+    public void testFileWithLargestPerimeter() {
+        // Put code here
+    }
+
+    // This method creates a triangle that you can use to test your other methods
+    public void triangle(){
+        Shape triangle = new Shape();
+        triangle.addPoint(new Point(0,0));
+        triangle.addPoint(new Point(6,0));
+        triangle.addPoint(new Point(3,6));
+        for (Point p : triangle.getPoints()){
+            System.out.println(p);
+        }
+        double peri = getPerimeter(triangle);
+        System.out.println("perimeter = "+peri);
+    }
+
+    // This method prints names of all files in a chosen folder that you can use to test your other methods
+    public void printFileNames() {
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            System.out.println(f);
+        }
+    }
+
+    public static void main (String[] args) {
+        PerimeterAssignmentRunner pr = new PerimeterAssignmentRunner();
+        pr.testPerimeter();
+    }
+}
