d2444023d94ec15a11635aa2f05e1db5a5fa4473
ashok997 <ashok997@gmail.com>
2020-06-02 15:14:21 -0500

    simple hash

---
+++ javascript/hashFunction.js
@@ -0,0 +1,10 @@
+function hash(key, arrLen) {
+    let total = 0;
+    let weirdPrime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * weirdPrime + value) % arrLen;
+    }
+    return total;
+  }
