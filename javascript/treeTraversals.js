7af18c10d3e9b2392f67f30d87c95a73877978d2
ashok997 <ashok997@gmail.com>
2020-06-29 21:17:55 -0500

    DFS added

---
+++ javascript/treeTraversals.js
@@ -0,0 +1,52 @@
+// TreeNode CLASS DEFINITION
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// MANUALLY CREATE A RANDOM TREE:
+
+/*
+         2
+       /   \
+      7     5
+     / \     \
+    2   6     9
+        /\    /
+       5 11  4
+  */
+
+const tree = new TreeNode(2); // root
+tree.left = new TreeNode(7);
+tree.right = new TreeNode(5);
+tree.left.left = new TreeNode(2);
+tree.left.right = new TreeNode(6);
+tree.left.right.left = new TreeNode(5);
+tree.left.right.right = new TreeNode(11);
+tree.right.right = new TreeNode(9);
+tree.right.right.left = new TreeNode(4);
+
+//console.log(tree);
+//post-order
+function dfs(root) {
+  const output = [];
+
+  const helper = (node) => {
+    if (node.left) {
+      helper(node.left);
+    }
+    if (node.right) {
+      helper(node.right);
+    }
+    output.push(node.val);
+  };
+  helper(root);
+
+  return output;
+}
+
+//console.log(dfs(tree));
