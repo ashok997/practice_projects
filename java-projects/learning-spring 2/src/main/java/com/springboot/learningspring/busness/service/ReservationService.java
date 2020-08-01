a93b090bdfbfa13c160db0a0a5dee1072e276d56
ashok997 <ashok997@gmail.com>
2020-08-01 16:04:05 -0500

    added hashmap of roomreservation in reservation service

--- java-projects/learning-spring 2/src/main/java/com/springboot/learningspring/busness/service/ReservationService.java
+++ java-projects/learning-spring 2/src/main/java/com/springboot/learningspring/busness/service/ReservationService.java
@@ -1,12 +1,16 @@
 package com.springboot.learningspring.busness.service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.learningspring.business.domain.RoomReservation;
+import com.springboot.learningspring.data.entity.Reservation;
+import com.springboot.learningspring.data.entity.Room;
 import com.springboot.learningspring.data.repository.GuestRepository;
 import com.springboot.learningspring.data.repository.ReservationRepository;
 import com.springboot.learningspring.data.repository.RoomRepository;
@@ -27,6 +31,22 @@ public class ReservationService {
 	}
 	
 	public List<RoomReservation> getRoomReservationForDate(Date date){
+		Iterable<Room> rooms = this.roomRepository.findAll();
+		
+		Map<Long, RoomReservation> roomReservationMap = new HashMap();
+		
+		rooms.forEach(room ->{
+			RoomReservation roomReservation = new RoomReservation();
+			roomReservation.setRoomId(room.getRoomID());
+			roomReservation.setRoomName(room.getRoomName());
+			roomReservation.setRoomNumber(room.getRoomNumber());
+			roomReservationMap.put(room.getRoomID(), roomReservation);
+			
+		});
+		
+		Iterable<Reservation> reservations = this.reservationRepository.findReservationByReservationDate(new java.sql.Date(date.getTime()));
+		
+	
 		return null;
 		
 	}
--- java-projects/learning-spring 2/src/main/java/com/springboot/learningspring/data/repository/ReservationRepository.java
+++ java-projects/learning-spring 2/src/main/java/com/springboot/learningspring/data/repository/ReservationRepository.java
@@ -1,5 +1,7 @@
 package com.springboot.learningspring.data.repository;
 
+import java.sql.Date;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.springboot.learningspring.data.entity.Reservation;
 
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long>{
+	Iterable<Reservation> findReservationByReservationDate(Date date);
 }
