aad41280ab3ee5420d02f34cd8d6ea948ecf1db9
ashok997 <ashok997@gmail.com>
2020-08-04 23:38:49 -0500

    added roomreservation controller

--- java-projects/learning-spring 2/src/main/java/com/springboot/learningspring/web/RoomReservationWebController.java
+++ java-projects/learning-spring 2/src/main/java/com/springboot/learningspring/web/RoomReservationWebController.java
@@ -1,5 +1,37 @@
 package com.springboot.learningspring.web;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springboot.learningspring.busness.service.ReservationService;
+import com.springboot.learningspring.business.domain.RoomReservation;
+
+@Controller
+@RequestMapping("/reservations")
 public class RoomReservationWebController {
+	private final ReservationService reservationService;
+	
+	
+	@Autowired
+	public RoomReservationWebController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+
+
+
+	@GetMapping
+	public String getReservations(@RequestParam(value="date", required = false)String dateString, Model model) {
+		Date date = DateUtils.createDateFromDateString(dateString);
+		List<RoomReservation> roomReservations = this.reservationService.getRoomReservationForDate(date);
+		model.addAttribute("roomReservations", roomReservations);
+		return "reservations";
+	}
 
 }
